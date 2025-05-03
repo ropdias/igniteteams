a93a59be354a0d318cbcc58b385707a8f2966a49
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-02 23:48:18 -0300

    add Header component with a .png image

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,10 @@
 import { Container } from './styles'
+import { Header } from '@components/Header'
 
 export function Groups() {
-  return <Container></Container>
+  return (
+    <Container>
+      <Header />
+    </Container>
+  )
 }
