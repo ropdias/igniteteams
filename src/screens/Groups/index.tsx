bdca2e44cfd1ca95276e047bd9885c8302b1d65d
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-03 16:12:04 -0300

    create Highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from './styles'
 import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   )
 }
