f074287a45c09cd10121be2a96ad6d33301df435
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-04 13:08:13 -0300

    create screen NewGroup

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 
 import theme from './src/theme'
 
-import { Groups } from '@screens/Groups'
+import { NewGroup } from '@screens/NewGroup'
 import { Loading } from '@/components/Loading'
 
 export default function App() {
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   )
 }
