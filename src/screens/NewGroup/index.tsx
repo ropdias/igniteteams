04568f4ee1f3ffe96ade46c096aeefe7ebd7ff41
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-06 18:17:17 -0300

    pass groups params information from NewGroup screen to Players screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Container, Content, Icon } from './styles'
 
@@ -7,10 +8,11 @@ import { Highlight } from '@components/Highlight'
 import { Input } from '@/components/Input'
 
 export function NewGroup() {
+  const [group, setGroup] = useState('')
   const navigation = useNavigation()
 
   function handleNew() {
-    navigation.navigate('players', { group: 'Rocket' })
+    navigation.navigate('players', { group })
   }
 
   return (
@@ -25,7 +27,7 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
