84cdd9acbee76d591919f9d4860d1b292bd11013
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-06 22:02:49 -0300

    handle exceptions when trying to create a new group displaying Alerts

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { Alert } from 'react-native'
+
+import { AppError } from '@utils/AppError'
+
 import { Container, Content, Icon } from './styles'
 
 import { groupCreate } from '@/storage/group/groupCreate'
@@ -15,10 +19,18 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
       await groupCreate(group)
       navigation.navigate('players', { group })
     } catch (error) {
-      console.log(error)
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+        console.log(error)
+      }
     }
   }
 
